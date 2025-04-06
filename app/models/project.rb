416f76ddd24ee2c368343564413c8b15077b42af
rohan1478 <muhammadrohanghauri@gmail.com>
2025-04-06 21:03:02 +0500

    Added stimulus JS and some UI improvements

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,8 @@ class Project < ApplicationRecord
     enum :status, { pending: 0, in_progress: 1, completed: 2 }
 
     accepts_nested_attributes_for :project_events, allow_destroy: false, reject_if: :all_blank
+
+    validates :title, presence: true, length: { maximum: 255 } 
+    validates :status, presence: true 
+    validates :description, presence: true, length: { maximum: 255 }
 end
